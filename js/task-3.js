aa35583d98537ccfcfc785eef2d51f3e1f539fe2
Vlad_Deineko <vladd1904@gmail.com>
2025-02-09 22:20:23 +0200

    :D

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function checkForSpam(message){
     const lowerCaseMessage = message.toLowerCase();
-    return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")
+    return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("saleki")
 }
 
 console.log(checkForSpam("Latest technology news"));
